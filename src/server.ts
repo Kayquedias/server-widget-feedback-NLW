230cb79551fdcb387db86e64c314f64144463bcb
Kayquedias <kayquemtd@gmail.com>
2022-10-19 15:00:59 -0300

    add port env variable

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { routes } from './routes';
 import cors from 'cors'
+import { env } from 'process';
 
 const app = express();
 
@@ -8,6 +9,6 @@ app.use(cors());
 app.use(express.json()); // Vai verificar antes da requisição se tem algum body em JSON
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("http server running man!");
 });
